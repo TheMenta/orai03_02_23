23bdabb88daa521f186013a5c1eaccd435199605
thementa <thementaaa@gmail.com>
2023-03-02 16:05:00 +0100

    Merge branch 'main' of https://github.com/TheMenta/orai03_02_23 into Main

--- SzamolosFeladatok/src/Ciklusok/szamok.java
+++ SzamolosFeladatok/src/Ciklusok/szamok.java
@@ -1,4 +1,14 @@
 package Ciklusok;
 
 public class szamok {
+    for (int k = 2; k <= 3; k++)
+    {
+        for (int j = 2; j <= 3; j++)
+        {
+            for (int i = 2; i <= 4; i++)
+            {
+                System.out.println("" + k + j + i);
+            }
+        }
+    }
 }
